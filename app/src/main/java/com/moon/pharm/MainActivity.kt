97db2d603793b2c9806117b5d08784c529e4bf5e
ExpeditionMoon <dnjswon1025@gmail.com>
2025-12-27 16:26:50 +0900

    refactor(consult): Hilt 도입으로 ConsultViewModelFactory제거
    #7

--- app/src/main/java/com/moon/pharm/MainActivity.kt
+++ app/src/main/java/com/moon/pharm/MainActivity.kt
@@ -4,11 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.ui.platform.LocalContext
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.moon.pharm.consult.factory.ConsultViewModelFactory
-import com.moon.pharm.consult.viewmodel.ConsultViewModel
 import com.moon.pharm.ui.screen.EntryPointScreen
 import com.moon.pharm.ui.theme.PharmMasterTheme
 import dagger.hilt.android.AndroidEntryPoint
